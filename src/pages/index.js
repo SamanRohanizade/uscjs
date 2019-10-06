aba63a7a9c416850518417755fd6f28cadf397f0
Saman <saman.rohanizade@gmail.com>
2019-10-06 17:13:16 +0330

    change layout's structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "src/components/layout"
+// import Image from "../components/image"
+import SEO from "src/components/seo"
+import Header from "src/components/header"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <Header></Header>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
+    </div> */}
   </Layout>
 )
 
