df49093b39e1107ebb664f8fd0b09d35cd790ec8
Saman <saman.rohanizade@gmail.com>
2019-10-06 22:27:08 +0330

    add icon to navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.scss"
 import bookImage from 'src/images/YDKJS.png'
+import logo from 'src/images/icon.png'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -19,22 +20,21 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header title="JavaScript" headerBottomImage={bookImage}>
-        <nav className="navbar navbar-expand-sm bg-transparent navbar-light fixed-top">
-          <a className="navbar-brand" href="#">
-            {data.site.siteMetadata.title}
-          </a>
-          
-          <ul className="navbar-nav">
+        <nav className="navbar navbar-expand-sm bg-transparent navbar-light fixed-top justify-content-between">
+          <ul className="navbar-nav pr-0">
             <li className="nav-item">
-              <a className="nav-link" href="#">Link 1</a>
+              <a className="nav-link" href="#">صفحه اصلی</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link 2</a>
+              <a className="nav-link" href="#">درباره دوره</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link 3</a>
+              <a className="nav-link" href="#">سرفصل‌های دوره</a>
             </li>
           </ul>
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="logo" width="45"/>
+          </a>
         </nav>
       </Header>
       <div className="container">{children}</div>
