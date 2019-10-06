41ff5d171ccf311c6d5a24f7dda6893190cb71ee
Saman <saman.rohanizade@gmail.com>
2019-10-06 16:39:54 +0330

    add and configure sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `USCJS`,
+    description: 'دوره آموزشی عمیق جاوااسکریپت!',
+    author: `Saman Rohanizade`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-sass',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
