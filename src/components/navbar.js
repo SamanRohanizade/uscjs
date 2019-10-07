69562bf5f992fc99d399ae89129cef5ebe91ed62
Saman <saman.rohanizade@gmail.com>
2019-10-07 13:21:54 +0330

    add about course section

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,10 +5,10 @@ const Navbar = ({ items, allowCollapse, logo }) => {
   let navClassNames = ['navbar-nav', 'pr-0', (allowCollapse ? '' : 'flex-row')];
 
   return (
-    <nav className="navbar navbar-expand-sm bg-transparent navbar-light justify-content-between">
+    <nav className="navbar navbar-expand-sm bg-primary navbar-light fixed-top justify-content-between">
       <ul className={navClassNames.join(' ')}>
         {items.map(i => (
-          <li className="nav-item ml-3">
+          <li className="nav-item ml-3" key={i.text}>
             <Link className="nav-link" to={i.to}>{i.text}</Link>
           </li>
         ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import React from "react"
 import Layout from "src/components/layout"
 // import Image from "../components/image"
 import SEO from "src/components/seo"
-import Header from "src/components/header"
+import styles from "./index.module.scss"
+import jsBagImage from 'src/images/js-bag.png'
+import samanImage from 'src/images/saman.jpg'
 
 const IndexPage = () => (
   <Layout>
@@ -10,26 +12,26 @@ const IndexPage = () => (
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+    <div className={styles.main}>
+      <h2 className="heading mx-auto">درباره این دوره</h2>
+    </div>
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-md-6 text-center">
+          <img src={jsBagImage} alt="" draggable="false" style={{ width: '75%' }}/>
+        </div>
+        <div className="col-md-6 d-flex align-items-center">
+          <p className="text-muted">
+          چند وقتی هست که به خاطر کامیونیتی فعال جاوااسکریپت، هر روز با تعداد زیادی ابزار جاوااسکریپتی مثل... روبه‌رو می‌شیم. و حتما دوره‌های "پروژه محور" زیادی که برای این ابزارها وجود دارند رو دیدید.
+
+اما در این دوره برعکس خیلی از دوره‌های موجود، مکانیزم‌های خود جاوااسکریپت مثل و ... رو به صورت عمیق بررسی می‌کنیم.
+
+در انتهای این دوره فقط مهارت کار با یک ابزار را کسب نمی‌کنیم. بلکه دانش پایه‌ای عمیقی به دست میاریم که برای یادگیری راحت‌تر همه‌ی ابزارهای جاوااسکریپتی، مصاحبه‌های شغلی، نوشتن کد با کیفیت‌تر و ... به کارمون میاد
+          </p>
+        </div>
+      </div>
+
+    </div>
   </Layout>
 )
 
