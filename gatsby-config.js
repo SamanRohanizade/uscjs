73364b9094846114248656bed83101d9cc65b35a
Saman <saman.rohanizade@gmail.com>
2019-10-08 01:12:55 +0330

    add & config transfer-remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
     `gatsby-plugin-root-import`,
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-sass',
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
