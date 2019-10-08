14b1acc41b0b8218d4b7f645c8502286ff80b11f
Saman <saman.rohanizade@gmail.com>
2019-10-08 21:28:41 +0330

    query all posts title and show in inside the drawer

--- src/components/sideDrawer.js
+++ src/components/sideDrawer.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import {Link} from 'gatsby'
 import styles from './sideDrawer.module.scss'
 
-const SideDrawer = ({ width, isOpen }) => {
+const SideDrawer = ({ width, isOpen, items }) => {
   let sideDrawerStyles = [styles.sidebar, (isOpen ? styles.open : '')]
   return (
     <aside className={sideDrawerStyles.join(" ")} style={{ width }}>
-      
+      <ul className={styles.sidebarList}>
+        {items.map(i => (
+          <li key={i.title}>
+            <Link to={i.path} activeClassName="text-primary">
+              {i.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </aside>
   )
 }
--- src/layouts/postsLayout.js
+++ src/layouts/postsLayout.js
@@ -1,11 +1,31 @@
 import React, {useState} from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import Navbar from "src/components/navbar"
 import SideDrawer from "src/components/sideDrawer"
 import BurgerMenuToggler from "src/components/burgerMenuToggler"
 import "./layout.scss"
 
-const PostsLayout = ({children}) => {
+const PostsLayout = ({ children }) => {
   let [sideDrawerVisibility, setSideDrawerVisibility] = useState(true);
+  let data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              path
+            }
+          }
+        }
+      }
+    }
+  `);
+  let sideDrawerItems = data.allMarkdownRemark.edges.map(i => {
+    let item = {};
+    ({title: item.title, path: item.path} = i.node.frontmatter);
+    return item;
+  });
 
   let navbarItems = [
     { text: 'درباره دوره', to: '/' },
@@ -25,7 +45,11 @@ const PostsLayout = ({children}) => {
         items={navbarItems}
         Toggler={toggler}
       ></Navbar>
-      <SideDrawer width="250px" isOpen={sideDrawerVisibility}></SideDrawer>
+      <SideDrawer
+        width="250px"
+        isOpen={sideDrawerVisibility}
+        items={sideDrawerItems}
+      ></SideDrawer>
       <div className={sideDrawerClassNames.join(' ')}>
         {children}
       </div>
