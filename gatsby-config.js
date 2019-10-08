101a696e4fef9643e1a01e4d54584e117d7c5978
Saman <saman.rohanizade@gmail.com>
2019-10-08 23:01:56 +0330

    change manifest and add gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,17 +28,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `uscjs`,
+        short_name: `دوره آموزشی عمیق جاوااسکریپت`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#7ba9d8`,
+        theme_color: `#7ba9d8`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
