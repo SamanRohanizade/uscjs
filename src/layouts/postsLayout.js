61ab6513b795e081deb8538d9b230d42902033bf
Saman <saman.rohanizade@gmail.com>
2019-10-08 20:07:58 +0330

    use toggler and drawer in postsLayout

--- src/layouts/postsLayout.js
+++ src/layouts/postsLayout.js
@@ -1,22 +1,32 @@
 import React, {useState} from 'react'
 import Navbar from "src/components/navbar"
 import SideDrawer from "src/components/sideDrawer"
+import BurgerMenuToggler from "src/components/burgerMenuToggler"
 import "./layout.scss"
 
 const PostsLayout = ({children}) => {
+  let [sideDrawerVisibility, setSideDrawerVisibility] = useState(true);
+
   let navbarItems = [
     { text: 'درباره دوره', to: '/' },
     { text: 'سرفصل‌های دوره', to: '/topics/basics' },
-  ]
+  ];
+  let sideDrawerClassNames = ['main', 'mt-5', 'pt-5', 'px-5', (sideDrawerVisibility ? 'with-sidebar' : '')];
+
+  let toggler = (<BurgerMenuToggler
+      active={sideDrawerVisibility}
+      onToggle={() => setSideDrawerVisibility(!sideDrawerVisibility)}
+    ></BurgerMenuToggler>)
 
   return (
     <>
       <Navbar
         allowCollapse={false}
         items={navbarItems}
+        Toggler={toggler}
       ></Navbar>
-      <SideDrawer width="250px" isOpen></SideDrawer>
-      <div className="main with-sidebar mt-5 pt-5 px-5">
+      <SideDrawer width="250px" isOpen={sideDrawerVisibility}></SideDrawer>
+      <div className={sideDrawerClassNames.join(' ')}>
         {children}
       </div>
     </>
