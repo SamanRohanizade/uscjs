3da666f59bd54b8a795f765d4f88d34c603d34bc
Saman <saman.rohanizade@gmail.com>
2019-10-08 20:07:35 +0330

    add toggler slot to navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Navbar = ({ items, allowCollapse, logo }) => {
+const Navbar = ({ items, allowCollapse, logo, Toggler }) => {
   let navClassNames = ['navbar-nav', 'pr-0', (allowCollapse ? '' : 'flex-row')];
 
   return (
@@ -9,13 +9,18 @@ const Navbar = ({ items, allowCollapse, logo }) => {
       className="navbar navbar-expand-sm bg-primary navbar-light fixed-top justify-content-between"
       style={{ minHeight: '3.5rem' }}  
     >
-      <ul className={navClassNames.join(' ')}>
-        {items.map(i => (
-          <li className="nav-item ml-3" key={i.text}>
-            <Link className="nav-link" activeClassName="active" to={i.to}>{i.text}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="d-flex">
+        <div className="toggler mt-1 ml-4">
+          {Toggler}
+        </div>
+        <ul className={navClassNames.join(' ')}>
+          {items.map(i => (
+            <li className="nav-item ml-2" key={i.text}>
+              <Link className="nav-link" activeClassName="active" to={i.to}>{i.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       
       {logo ? <a className="navbar-brand" href="#">
         <img src={logo} alt="logo" width="45"/>
