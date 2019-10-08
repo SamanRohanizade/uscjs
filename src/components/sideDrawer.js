1145e96d83ff55c7c26475b16aa01c932a3305fd
Saman <saman.rohanizade@gmail.com>
2019-10-08 18:51:49 +0330

    add sideDrawer component

---
+++ src/components/sideDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './sideDrawer.module.scss'
+
+const SideDrawer = ({ width }) => {
+  return (
+    <aside className={styles.sidebar} style={{ width }}>
+      
+    </aside>
+  )
+}
+
+export default SideDrawer;
